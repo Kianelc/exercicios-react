d9be2a0d93cdecf849f2291be32e91054d52967a
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-14 22:01:04 -0300

    Importando multiplos elementos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PrimeiroComponente from './componentes/PrimeiroComponente'
+import {CompA, CompB as ComponenteB} from './componentes/SegundoComponente'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
         <h1>Olá Mundo React!</h1>
-        <PrimeiroComponente></PrimeiroComponente>
+        <PrimeiroComponente valor="e primeira" outroValor="propriedade"></PrimeiroComponente>
+        <CompA compa="Segundo Componente"></CompA>
+        <ComponenteB compb="Terceiro Componente"></ComponenteB>
     </div>
 , elemento)
