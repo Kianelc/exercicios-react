1c7212140e7d782d0dd1fd759fdf8410b1f92a5f
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-15 21:27:40 -0300

    Ajuste no html

--- src/componentes/SegundoComponente.jsx
+++ src/componentes/SegundoComponente.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-// Solução 01 para retornar multiplos elementos
+// Solução 01 para retornar multiplos elementos (array)
 const CompA = props => [
-    <h1>Primeiro componente {props.compa}</h1>,
-    <div>colocando array</div>
+    <h1>{props.compa}</h1>,
+    <small>Array</small>
 ]
 
-// Solução 02 para retornar multiplos elementos
+// Solução 02 para retornar multiplos elementos (fragment)
 const CompB = props =>
     <React.Fragment>
-        <h1>Segundo componente {props.compb}</h1>
-        <div>colocando fragment</div>
+        <h1>{props.compb}</h1>
+        <small>Fragment</small>
     </React.Fragment>
 
 export {CompA, CompB}
