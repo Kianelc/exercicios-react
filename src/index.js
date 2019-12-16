b7d56e28670a222d9c64402b3c66aa978062b1b3
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-15 21:27:56 -0300

    Reestruturação do index e inclusão de novos componentes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PrimeiroComponente from './componentes/PrimeiroComponente'
-import {CompA, CompB as ComponenteB} from './componentes/SegundoComponente'
+import { CompA, CompB as ComponenteB } from './componentes/SegundoComponente'
+import FamiliaCasagrande from './componentes/FamiliaCasagrande'
+import Membro from './componentes/Membro'
+import Familia from './componentes/Familia'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
         <h1>Olá Mundo React!</h1>
-        <PrimeiroComponente valor="e primeira" outroValor="propriedade"></PrimeiroComponente>
+        <strong>Relação Entre Componente 01</strong>
+        <FamiliaCasagrande></FamiliaCasagrande>
+        <br />
+        <br />
+        <strong>Relação Entre Componente 02</strong>
+        <Familia>
+            <Membro nome="Lucia" sobrenome="Casagrande"></Membro>
+            <Membro nome="Harry" sobrenome="Casagrande"></Membro>
+        </Familia>
+        <br />
+        <br />
+        <strong>Primeiro Componente</strong>
+        <PrimeiroComponente valor="Primeira" outroValor="propriedade"></PrimeiroComponente>
+        <br />
+        <br />
+        <strong>Retornar Multiplos Elementos</strong>
         <CompA compa="Segundo Componente"></CompA>
         <ComponenteB compb="Terceiro Componente"></ComponenteB>
     </div>
-, elemento)
+    , elemento)
