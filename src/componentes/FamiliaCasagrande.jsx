a762c07acb4549b27c0b72618fd93bdb3f5a470e
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-15 21:27:19 -0300

    Relação entre componentes 01

---
+++ src/componentes/FamiliaCasagrande.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Membro from './Membro'
+
+export default props =>
+    <div>
+        <Membro nome="Kiane" sobrenome="Casagrande"></Membro>
+        <Membro nome="Kamille" sobrenome="Casagrande"></Membro>
+        <Membro nome="Nazarete" sobrenome="Casagrande"></Membro>
+        <Membro nome="Jurandir" sobrenome="Casagrande"></Membro>
+    </div>
