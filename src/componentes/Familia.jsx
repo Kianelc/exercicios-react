08a96db6a022974ba677fcd58d202ce0e0fd0495
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-15 22:19:37 -0300

    Propagrando props para componentes filhos 2

--- src/componentes/Familia.jsx
+++ src/componentes/Familia.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default props =>
     <div>
-        {React.cloneElement(props.children, 
-            {sobrenome: props.sobrenome})}
+        {/* {...props} => cria um novo objeto, passa as propiedades dentro
+        do objeto e usa a notação spred para criar um objeto clonado */}
+        {React.cloneElement(props.children, {...props})}
+        {/* React.cloneElement(props.children, 
+            {sobrenome: props.sobrenome}) */}
     </div>
