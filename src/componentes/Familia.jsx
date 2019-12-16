b6895f7ddccff4a9499a784007128547e93d0492
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-15 22:00:17 -0300

    Propagrando props para um unico elemento

--- src/componentes/Familia.jsx
+++ src/componentes/Familia.jsx
@@ -2,5 +2,6 @@ import React from 'react'
 
 export default props =>
     <div>
-        {props.children}
+        {React.cloneElement(props.children, 
+            {sobrenome: props.sobrenome})}
     </div>
