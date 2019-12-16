98628065819a05e9886c8255842b9170ccc5c418
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-15 21:59:55 -0300

    Comunicação direta entre componentes (pai e filho)

--- src/componentes/FamiliaCasagrande.jsx
+++ src/componentes/FamiliaCasagrande.jsx
@@ -3,8 +3,8 @@ import Membro from './Membro'
 
 export default props =>
     <div>
-        <Membro nome="Kiane" sobrenome="Casagrande"></Membro>
-        <Membro nome="Kamille" sobrenome="Casagrande"></Membro>
-        <Membro nome="Nazarete" sobrenome="Casagrande"></Membro>
-        <Membro nome="Jurandir" sobrenome="Casagrande"></Membro>
+        <Membro nome="Kiane" sobrenome={props.sobrenome}></Membro>
+        <Membro nome="Kamille" sobrenome={props.sobrenome}></Membro>
+        <Membro nome="Nazarete" sobrenome={props.sobrenome}></Membro>
+        <Membro nome="Jurandir" sobrenome={props.sobrenome}></Membro>
     </div>
