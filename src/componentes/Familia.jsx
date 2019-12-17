6a2137711280f2fb3e90cc97f24f7475aa1705e5
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-16 23:32:53 -0300

    Importando a função do utils

--- src/componentes/Familia.jsx
+++ src/componentes/Familia.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {filhosComProps} from '../utils/utils'
 
 export default props =>
     <div>
+        {filhosComProps(props)}
         {/* {...props} => cria um novo objeto, passa as propiedades dentro
         do objeto e usa a notação spred para criar um objeto clonado */}
-        {React.cloneElement(props.children, {...props})}
+        {/*React.Children.map(props.children, child => {
+            return React.cloneElement(props.children, {...props})
+        })*/}
         {/* React.cloneElement(props.children, 
             {sobrenome: props.sobrenome}) */}
     </div>
