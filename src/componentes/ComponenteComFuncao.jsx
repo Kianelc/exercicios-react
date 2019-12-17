eebaf4ac289cc3e2ba75eee50d824044466911ea
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-17 20:25:59 -0300

    Componentes com função

--- src/componentes/ComponenteComFuncao.jsx
+++ src/componentes/ComponenteComFuncao.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const aprovados = ['Julia', 'Gustavo', 'Lucas', 'Isabela']
-
 export default props => {
+    const aprovados = ['Julia', 'Gustavo', 'Lucas', 'Isabela']
     const gerarItens = itens => {
-        
+        return itens.map(item => <li>{item}</li>)
     }
+
+    return (
+        <ul>{gerarItens(aprovados)}</ul>
+    )
 }
