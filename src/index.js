0c382102b509d8e4efe42c1978a9536e0260af2f
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-18 23:12:16 -0300

    Chamada dos componentes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,21 @@ import FamiliaCasagrande from './componentes/FamiliaCasagrande'
 import Membro from './componentes/Membro'
 import Familia from './componentes/Familia'
 import ComponenteComFuncao from './componentes/ComponenteComFuncao'
+import Pai from './componentes/Pai'
+import ComponenteClasse from './componentes/ComponenteClasse'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
         <h1>Olá Mundo React!</h1>
+        <strong>Componente de Classe</strong>
+        <ComponenteClasse valor="Classe!"></ComponenteClasse>
+        <br/>
+        <br/>
+        <strong>Comunicação Indireta entre Componentes</strong>
+        <Pai></Pai>
+        <br/>
+        <br/>
         <strong>Componente com função</strong>
         <ComponenteComFuncao></ComponenteComFuncao>
         <br/>
