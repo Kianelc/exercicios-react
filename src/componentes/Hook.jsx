aba25e23bcf44d4580a203b8409d96f6e0c0f8c9
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2019-12-26 23:56:48 -0300

    React Hooks

---
+++ src/componentes/Hook.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react'
+
+export default props => {
+    const [contador, setContador] = useState(100)
+    const [status, setStatus] = useState('Par')
+
+    useEffect(() => {
+        contador % 2 === 0 ? setStatus('Par') : setStatus('Impar')
+    })
+    return (
+        <div>
+            <h1>{contador}</h1>
+            <h1>{status}</h1>
+            <button onClick={() => setContador(contador + 1)}>INC</button>
+        </div>
+    )
+}
